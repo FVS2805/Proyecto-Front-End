8d5b49e0395edfdc7ce90df0c69fef2fa026b6ca
FVS2805 <f28vs01@gmail.com>
2025-05-05 23:04:32 -0400

    Tarea Hecha

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ document.getElementById("studentForm").addEventListener("submit",function(e){
     e.preventDefault();
     const name=document.getElementById("name").value.trim();
     const lastName=document.getElementById("lastName").value.trim();
+    const Fecha = document.getElementById("Fecha").value.trim();
     const grade=parseFloat(document.getElementById("grade").value);
 
-    if(!name || !lastName || isNaN(grade) || grade<1 || grade>7){
+    if(!name || !lastName || !Fecha || isNaN(grade) || grade<1 || grade>7){
         alert("Error al ingresar Datos")
          return
     }
-    const student={name,lastName,grade}
+    const student={name,lastName,Fecha,grade}
 
     students.push(student);
     
@@ -26,6 +27,7 @@ document.getElementById("studentForm").addEventListener("submit",function(e){
             row.innerHTML=`
                 <td>${student.name}</td>
                 <td>${student.lastName}</td>
+                <td>${student.Fecha}</td>
                 <td>${student.grade}</td>`;
             tableBody.appendChild(row);
         }
